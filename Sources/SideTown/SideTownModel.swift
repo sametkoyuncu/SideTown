258aa71c2006f3b693fd7a7aacbd29f7782f9a41
Samet Koyuncu <sametkoyuncu@live.com>
2022-11-14 10:51:31 +0300

    Add public initializer to MenuConfig

--- Sources/SideTown/SideTownModel.swift
+++ Sources/SideTown/SideTownModel.swift
@@ -24,6 +24,13 @@ public enum NavBarStatus {
 public struct MenuConfig {
     var vc: UIViewController
     var customView: UIView
-    var position: MenuPosition = .left
-    var backgroundColor: UIColor = .darkGray.withAlphaComponent(0.7)
+    var position: MenuPosition
+    var backgroundColor: UIColor
+    
+    public init(vc: UIViewController, customView: UIView, position: MenuPosition = .left, backgroundColor: UIColor = .darkGray.withAlphaComponent(0.7)) {
+        self.vc = vc
+        self.customView = customView
+        self.position = position
+        self.backgroundColor = backgroundColor
+    }
 }
